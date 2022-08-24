d66d81b340bf0ac35674f8355f4cb73491d28580
Clemente Acosta <clems.acosta@gmail.com>
2022-08-23 20:10:37 -0500

    add toggle tabs feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,23 @@
 const menuButton = document.querySelector('#menu')
 const navContainer = document.querySelector('.navbar__container')
+const closeButton = document.querySelector('#close-btn')
+const navbarTitle = document.querySelectorAll('.navbar__title')
 
 menuButton.addEventListener('click', () => {
-  navContainer.classList.toggle('hidden')  
+  navContainer.classList.remove('hidden')  
+})
+
+closeButton.addEventListener('click', () => {
+  navContainer.classList.add('hidden')
+})
+
+navbarTitle.forEach(title => {
+  title.addEventListener('click', ({target}) => {
+    const ulSibling = target.nextSibling.nextSibling
+    const arrowImg = target.firstElementChild
+    
+    arrowImg.classList.toggle('turn-up')
+    ulSibling.classList.toggle('hidden')
+
+  })
 })
